93fd3ea446a2d11fcdff3e5fd4e8ebfb64caef1f
Nazam-Kalsi <nzamkalsi@gmail.com>
2025-04-22 10:38:56 +0530

    learn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,30 @@
   import './App.css'
 import ThemeToggler from './components/ThemeToggler';
 import { useMouse } from './hooks/useMouse';
-import { useOnline } from './hooks/useOnline';
-import { useSocket } from './hooks/useSocket'
+// import { useOnline } from './hooks/useOnline';
+// import { useSocket } from './hooks/useSocket'
 
 function App() {
-  const {socket} = useSocket();
-  const {isOnline} = useOnline();
+  // const {socket} = useSocket();
+  // const {isOnline} = useOnline();
   const {mouseData} = useMouse();
 // console.log("socket : ",socket);
 // console.log("isOnline : ",isOnline);
   return (
     <>
-    {<p>
-      JSON.stringify(mouseData)
+    <p>
+      {JSON.stringify(mouseData)}      
     </p> 
-      }
-    <ThemeToggler/>
+    <div className='border w-screen h-8 flex justify-center'>
+      <div className='flex flex-grow justify-end'>
+      <div className='h-full w-10 border bg-red-700'></div>
+      </div>
+      <div className='flex flex-grow justify-end pr-4'>
+      <div className='h-full w-10 border bg-blue-700'></div>
+      </div>
+    </div>
+    
+    <ThemeToggler className=''/>
       {/* <div className={`circle`} style={{
             position: 'absolute',
             top: mouseData?.y || 0,
--- client/src/components/ThemeToggler.tsx
+++ client/src/components/ThemeToggler.tsx
@@ -1,22 +1,57 @@
-import { LucideMoon, SunDim } from 'lucide-react'
-import { useState } from 'react'
+import { LucideMoon, SunDim } from "lucide-react";
+import { useState, useRef, useEffect } from "react";
+import { flushSync } from "react-dom";
 
 type Props = {
-  className?:string
-}
+  className?: string;
+};
 
-function ThemeToggler({className}: Props) {
-  const [isDark,setIsDark] = useState<boolean>(false);
+function ThemeToggler({ className }: Props) {
+  const [isDark, setIsDark] = useState<boolean>(false);
+  const ref = useRef<HTMLButtonElement | null>(null);
+  
+  const change = async() => {
+    if(!ref.current)return;
+    await document.startViewTransition(() => {
+      flushSync(() => {
+        const dark = document.documentElement.classList.toggle("dark");
+        setIsDark(dark);
+      });
+    }).ready;
+    
+    const { top, left, width, height } = ref.current.getBoundingClientRect() ;
+    const x = left + width / 2;
+    const y = top + height / 2;
+      const right = window.innerWidth - left;
+      const bottom = window.innerHeight - top;
+      const maxRadius = Math.hypot(
+        Math.max(left, right),
+        Math.max(top, bottom),
+      );
+    document.documentElement.animate(
+      {
+        clipPath:[
+          `circle(0px at ${x}px ${y}px)`,
+          `circle(${Number(maxRadius)}px at ${x}px ${y}px)`,
+        ]
+      },
+      {
+        duration: 1000,
+        easing: 'ease-in-out',
+        pseudoElement: '::view-transition-new(root)',
+      }
+    );
+  };
 
-  const change = () => {
-    const dark = document.documentElement.classList.toggle('dark');
-    setIsDark(dark);
-  }
-
+  // useEffect(()=>{
+  //   change();
+  // },[])
 
   return (
-    <button onClick={change} className={`${className}`}>{isDark?<SunDim/>:<LucideMoon/>}</button>
-  )
+    <button ref={ref} onClick={change} className={`${className}`}>
+      {isDark ? <SunDim /> : <LucideMoon />}
+    </button>
+  );
 }
 
-export default ThemeToggler
+export default ThemeToggler;
